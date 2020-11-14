b07aeac916ea1af610ce06cb06887f5f53f872f1
James Petley <jwpetley@gmail.com>
2020-11-14 21:02:24 +0000

    Set up a new branch for changing CSS and improving look of the pages

--- EduFlixApp/__init__.py
+++ EduFlixApp/__init__.py
@@ -31,7 +31,7 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/')
     def index():
-        return render_template("base.html")
+        return render_template("index.html")
 
 
     from . import db
