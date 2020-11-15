3f8ca40ce9e9e4342782506edb606d9eb09b771c
EC2 Default User <ec2-user@ip-172-31-27-242.eu-west-2.compute.internal>
2020-11-15 03:36:32 +0000

    Fixes for use on EC2. Sorry about hard coding keys

--- EduFlixApp/S3upload.py
+++ EduFlixApp/S3upload.py
@@ -22,7 +22,9 @@ def upload_file_from_path(file_name,file_path,user_id):
     #sys('export AWS_ACCESS_KEY_ID=AKIA6DKSR4M556OULTXU')
 
     # Upload the file
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client('s3',aws_access_key_id=ACCESS_KEY,
+                aws_secret_access_key=SECRET_KEY,
+                    aws_session_token=SESSION_TOKEN)
     try:
         response = s3_client.upload_file(object_upload, bucket_name, object_name)
     except ClientError as e:
@@ -45,7 +47,9 @@ def upload_file_object(file, username, title):
     bucket_name='eduflixvid'
     print(object_name)
     # Upload the file
-    s3_client = boto3.client('s3')
+    session = boto3.Session(aws_access_key_id='AKIA6DKSR4M556OULTXU',
+                aws_secret_access_key='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')
+    s3_client = session.client('s3')
     try:
         response = s3_client.upload_fileobj(file, bucket_name, object_name, ExtraArgs={'ACL':'public-read'})
     except ClientError as e:
---
+++ application.py
@@ -0,0 +1,8 @@
+
+from EduFlixApp import create_app
+
+application = create_app()
+
+if __name__ == "__main__":
+    application.debug = True    
+    application.run(host="0.0.0.0", port=80)
